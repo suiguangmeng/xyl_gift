65a731a23a4c33d81b761db58bc2adb33ebe596a
suiguangmeng <suiguangmeng@github.com>
2026-06-21 00:42:13 +0800

    修复手机端 Memories 翻页和背景问题：添加CSS定位、触摸事件支持

--- js/diy.js
+++ js/diy.js
@@ -5,9 +5,15 @@ $(function(){
         'navigationColor':['#fff'],
         'css3': true,
         'scrollingSpeed': 700,
-        'touchSensitivity': 5,
-        'responsiveWidth': 768,
-        'afterResponsive': function(isResponsive){
+        'touchSensitivity': 15,
+        'normalScrollElementTouchThreshold': 5,
+        'responsiveWidth': 0,
+        'fitToSection': true,
+        'scrollBar': false,
+        'autoScrolling': true,
+        'afterRender': function(){
+            // 页面渲染完成后确保背景显示
+            $('.bgcolor').css('opacity', '0.9');
         }
     });
 });
