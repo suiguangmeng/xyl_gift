e4cdcc69004520741b8344ae7e7f3ac6b1d4ec62
suiguangmeng <suiguangmeng@github.com>
2026-06-21 01:09:34 +0800

    修复 Memories 页面链接点击问题，添加链接点击事件处理

--- js/diy.js
+++ js/diy.js
@@ -11,9 +11,23 @@ $(function(){
         'fitToSection': true,
         'scrollBar': false,
         'autoScrolling': true,
+        'scrollOverflow': false,
+        // 允许链接点击
+        'onLeave': function(origin, destination, direction){
+            return true;
+        },
         'afterRender': function(){
             // 页面渲染完成后确保背景显示
             $('.bgcolor').css('opacity', '0.9');
+        }
+    });
+    
+    // 确保链接可以点击
+    $('a').on('click touchend', function(e) {
+        var href = $(this).attr('href');
+        if (href && href !== '#' && href !== '') {
+            e.stopPropagation();
+            window.location.href = href;
         }
     });
 });
