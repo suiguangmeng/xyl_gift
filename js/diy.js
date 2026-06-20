92abd5cc5323b87f534f0af426730d3e5d559f31
suiguangmeng <suiguangmeng@github.com>
2026-06-21 00:08:54 +0800

    修复 Memories.html 翻页和背景问题

---
+++ js/diy.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('#dowebok').fullpage({
+        'navigation': true,
+        'navigationPosition':"left",
+        'navigationColor':['#fff'],
+        'css3': true,
+        'scrollingSpeed': 700,
+        'touchSensitivity': 5,
+        'responsiveWidth': 768,
+        'afterResponsive': function(isResponsive){
+        }
+    });
+});
